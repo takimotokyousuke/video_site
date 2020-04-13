79a94de2269fa3f271099ae880fa68cafa3feef6
ta07ki <60640482+takimotokyousuke@users.noreply.github.com>
2020-04-13 14:52:21 +0900

    修正しただけ
    これから表示させる

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_04_065709) do
+ActiveRecord::Schema.define(version: 2020_04_08_085750) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -29,9 +29,9 @@ ActiveRecord::Schema.define(version: 2020_04_04_065709) do
   create_table "videos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "text"
     t.integer "user_id"
     t.string "title", null: false
+    t.string "text", null: false
   end
 
 end
