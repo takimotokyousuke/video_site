a95cb31b93f3c39c885f5eea4503c43d042c489f
ta07ki <60640482+takimotokyousuke@users.noreply.github.com>
2020-04-15 16:57:18 +0900

    修正

--- db/schema.rb
+++ db/schema.rb
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 2020_04_08_085750) do
   end
 
   create_table "videos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.text "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
